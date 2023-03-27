dbba1845379aae05fddd1778eb43474b5b9f01fc
idbenitez <ivan@cmb.udl.cat>
2023-03-27 11:38:46 +0200

    AUC

--- Expresion_diferencial_logbase.R
+++ Expresion_diferencial_logbase.R
@@ -4,7 +4,6 @@
 ## Update
 ## Volcano plot habia un error en el codigo y no printaba el nombre de las proteinas
 
-
 expresion_diferencial_logbase <- function(datos, var_grupo, vars_ajuste, noms_proteines, names_id, logbase = 2,
                                           pval = 0.05, FC_limit = 1.15) {
   
@@ -53,18 +52,18 @@ expresion_diferencial_logbase <- function(datos, var_grupo, vars_ajuste, noms_pr
   
   ## ebayes: Empirical Bayes Statistics for Differential Expression  ##
   eBa <- eBayes(cont.fit)
-    # eBa$coefficients: contrast difference by groups
-    # eBa$p.value: pvalue of the contrast diffference by groups
-
+  # eBa$coefficients: contrast difference by groups
+  # eBa$p.value: pvalue of the contrast diffference by groups
+  
   ## Obtencion de la tabla de resultados con los ddCt y los p-values ##
   limma <- topTable(eBa, adjust.method = "fdr", number = length(noms_proteines), sort.by = "p" )
-    # logFC: estimate of the log2-fold-change corresponding to the effect or contrast (for topTableF there may be several columns of log-fold-changes)
-    # AveExpr: average log2-expression for the probe over all arrays and channels, same as Amean in the MarrayLM object
-    # t: moderated t-statistic
-    # P.Value: raw p-value
-    # adj.P.Value: adjusted p-value or q-value
-    # B: log-odds that the gene/protein is differentially expressed
-
+  # logFC: estimate of the log2-fold-change corresponding to the effect or contrast (for topTableF there may be several columns of log-fold-changes)
+  # AveExpr: average log2-expression for the probe over all arrays and channels, same as Amean in the MarrayLM object
+  # t: moderated t-statistic
+  # P.Value: raw p-value
+  # adj.P.Value: adjusted p-value or q-value
+  # B: log-odds that the gene/protein is differentially expressed
+  
   ## Seleccion de informacion a reportar [Delta-Delta-Ct algorithm (ddCt)]
   ddCt <- data.frame(rownames(limma), logbase^(limma$logFC), limma$P.Value, limma$adj.P.Val)
   colnames(ddCt) <- c("Names", "FC", "p.value", "FDR")
@@ -98,7 +97,7 @@ expresion_diferencial_logbase <- function(datos, var_grupo, vars_ajuste, noms_pr
       scale_colour_manual(values = cols) +
       ggtitle(label = "", subtitle = "") +
       geom_point(size = 2.5, alpha = 1, na.rm = T) +
-     # geom_text(show.legend = FALSE) + #hjust=0, vjust=0, 
+      # geom_text(show.legend = FALSE) + #hjust=0, vjust=0, 
       theme_bw(base_size = 14) +
       theme(legend.position = "top",
             legend.title = element_blank(),
@@ -110,7 +109,7 @@ expresion_diferencial_logbase <- function(datos, var_grupo, vars_ajuste, noms_pr
       geom_hline(yintercept = (-1)*log(pval), colour = "blue", linetype = "dashed") +
       geom_vline(xintercept = (-1)*log10(FC_limit), colour = "#990000", linetype = "dashed") +
       geom_vline(xintercept = log10(FC_limit), colour = "#990000", linetype = "dashed") +
-     # scale_y_continuous(trans = "log1p") + 
+      # scale_y_continuous(trans = "log1p") + 
       annotate("text", x = log10(FC_limit), y = 0, label = paste0("FC = ", FC_limit), hjust = 0.15, vjust = 1, angle=90, colour = "#990000")+ 
       annotate("text", x = -log10(FC_limit), y = 0, label = paste0("FC = ", round(1/FC_limit,2)), hjust = 0.15, vjust = -0.25, angle=90, colour = "#990000") +
       ggrepel::geom_text_repel(aes(x = x, y = y, label=names),show.legend=FALSE) 
@@ -123,7 +122,7 @@ expresion_diferencial_logbase <- function(datos, var_grupo, vars_ajuste, noms_pr
     #             : (-1)*log(max(ddCt$p.value[ddCt$FDR<0.20]))
     #  log2(Fold Change): (-1)*log2(1.15)
     #                   : log2(1.15)
-
+    
     ss <- data.frame(x = eBa$coefficients[,1], y = c(-1)*log(eBa$p.value[,1]), names = rownames(eBa$lods))
     ss$color <- ifelse(ss$y >= (-1)*log(pval_FDR0.20) & !is.na(pval_FDR0.20) & (ss$x < -(1)*log2(FC_limit) | ss$x >= log2(FC_limit)),"FDR<0.20",
                        ifelse(ss$y >= (-1)*log(pval) & (ss$x < -(1)*log2(FC_limit) | ss$x >= log2(FC_limit)),paste0("P-value<",pval),"Non-significant"))
@@ -134,7 +133,7 @@ expresion_diferencial_logbase <- function(datos, var_grupo, vars_ajuste, noms_pr
       scale_colour_manual(values = cols) +
       ggtitle(label = "", subtitle = "") +
       geom_point(size = 2.5, alpha = 1, na.rm = T) +
-     # geom_text(show.legend = FALSE) + #hjust=0, vjust=0, 
+      # geom_text(show.legend = FALSE) + #hjust=0, vjust=0, 
       theme_bw(base_size = 14) +
       theme(legend.position = "top",
             legend.title = element_blank(),
@@ -146,14 +145,24 @@ expresion_diferencial_logbase <- function(datos, var_grupo, vars_ajuste, noms_pr
       geom_hline(yintercept = (-1)*log(pval), colour = "blue", linetype = "dashed") +
       geom_vline(xintercept = (-1)*log2(FC_limit), colour = "#990000", linetype = "dashed") +
       geom_vline(xintercept = log2(FC_limit), colour = "#990000", linetype = "dashed") +
-    #  scale_y_continuous(trans = "log1p")  + 
+      #  scale_y_continuous(trans = "log1p")  + 
       annotate("text", x = log2(FC_limit), y = 0, label = paste0("FC = ", FC_limit), hjust = 0.15, vjust = 1, angle=90, colour = "#990000")+ 
       annotate("text", x = -log2(FC_limit), y = 0, label = paste0("FC = ", round(1/FC_limit,2)), hjust = 0.15, vjust = -0.25, angle=90, colour = "#990000")# + 
-     # ggrepel::geom_text_repel(aes(x = x, y = y, label=names),show.legend=FALSE) 
+    # ggrepel::geom_text_repel(aes(x = x, y = y, label=names),show.legend=FALSE) 
     
     if(!is.na(pval_FDR0.20)) bp  <- bp + geom_hline(yintercept = (-1)*log(pval_FDR0.20), colour = "darkgreen", linetype = "dashed")
   }
-
+  
+  aux <- data.frame(datos %>% dplyr::select(noms_proteines))
+  res <- apply(aux,2,
+               function(x){pROC::auc(datos[,var_grupo], x)})
+  res <- ifelse(res<0.5,
+                1-res,res)
+  res <- data.frame(Names =names(res),AUC = res)
+  ddCt <- merge(ddCt,res,by = "Names",all = TRUE)
+  
+  
+  
   ## Resultados a devolver  
   list(limma %>% mutate_if(is.numeric, round, digits=5),
        ddCt  %>% mutate_if(is.numeric, round, digits=3), 
